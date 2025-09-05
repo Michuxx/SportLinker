87796ca304cf8d1f979bf283e31438ee055911ab
Michu <104158118+Michuxx@users.noreply.github.com>
2025-09-05 13:21:57 +0000

    Created Offer section and headlines to it

--- SportLinkerFrontend/src/App.jsx
+++ SportLinkerFrontend/src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import Header from "./components/headerSection/Header";
 import WelcomeBanner from "./components/bannerSection/welcomeBanner";
+import OfferSection from "./components/offerSection/OfferSection";
+import OfferFilter from "./components/offerSection/offerFilter/OfferFilter";
 
 function App() {
   return (
     <div className="container">
       <Header />
       <WelcomeBanner />
+      <OfferSection>
+        <OfferFilter />
+      </OfferSection>
     </div>
   );
 }
---
+++ SportLinkerFrontend/src/components/offerSection/offerFilter/OfferFilter.jsx
@@ -0,0 +1,9 @@
+const OfferFilter = () => {
+  return (
+    <div className="filter-wrapper">
+      <h3>Filtruj oferty</h3>
+    </div>
+  );
+};
+
+export default OfferFilter;
