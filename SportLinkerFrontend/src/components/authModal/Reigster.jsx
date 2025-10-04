85c786cd1111e70f171fb9a58de21f83b6746b1a
Michuxx <104158118+Michuxx@users.noreply.github.com>
2025-10-04 12:05:34 +0200

    Added registration UI and update styles
    Added a registration form layout in Register.jsx with input fields for name, email, and password, including relevant icons and a submit button. Created basic styling for the registration header in register.css. Adjusted logo image width from 14rem to 12rem in logo.css for better visual balance.

--- SportLinkerFrontend/src/components/authModal/Reigster.jsx
+++ SportLinkerFrontend/src/components/authModal/Reigster.jsx
@@ -1,5 +1,46 @@
+import Button from "../component-items/button/button";
+import Input from "../component-items/input/Input";
+import InputField from "../component-items/inputField/InputField";
+import Logo from "../component-items/logo/Logo";
+import "./register.css";
+import { MdMailOutline } from "react-icons/md";
+import { GoLock } from "react-icons/go";
+import { LuUser } from "react-icons/lu";
+
 const Register = () => {
-  return <div className="register-wrapper">register</div>;
+  return (
+    <div className="register-wrapper">
+      <Logo />
+      <h2>Utwórz konto w SportLinker</h2>
+      <InputField label="Imię i nazwisko">
+        <Input
+          placeholder="John Doe"
+          type="text"
+          width={100}
+          icon={<LuUser color="rgb(156 163 175)" size="20px" />}
+        />
+      </InputField>
+      <InputField label="Email">
+        <Input
+          placeholder="John@example.com"
+          type="email"
+          width={100}
+          icon={<MdMailOutline color="rgb(156 163 175)" size="20px" />}
+        />
+      </InputField>
+      <InputField label="Hasło">
+        <Input
+          placeholder="••••••••"
+          type="password"
+          width={100}
+          icon={<GoLock color="rgb(156 163 175)" size="20px" />}
+        />
+      </InputField>
+      <Button style="loginButton" width={100}>
+        Utwórz konto
+      </Button>
+    </div>
+  );
 };
 
 export default Register;
