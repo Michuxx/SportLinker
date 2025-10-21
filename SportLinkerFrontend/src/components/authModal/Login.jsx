8b2dbe8ffac177db3d3599344a188054dcb7f7cc
Michu <104158118+Michuxx@users.noreply.github.com>
2025-10-21 14:51:38 +0000

    Validation of Register. simplified states in Login component.

--- SportLinkerFrontend/src/components/authModal/Login.jsx
+++ SportLinkerFrontend/src/components/authModal/Login.jsx
@@ -8,34 +8,41 @@ import Button from "../component-items/button/button";
 import { useState } from "react";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailErr, setEmailErr] = useState();
-  const [passwordErr, setPasswordErr] = useState();
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
 
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-  const onEmailChange = (e) => {
-    setEmail(e.target.value);
-    setEmailErr("");
-  };
-
-  const onPasswordChange = (e) => {
-    setPassword(e.target.value);
-    setPasswordErr("");
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prev) => ({ ...prev, [name]: value }));
+    setErrors((prev) => ({ ...prev, [name]: "" }));
   };
 
   const emailValidation = () => {
-    if (email.length === 0 || !emailRegex.test(email)) {
-      setEmailErr("Twój email jest nieprawidłowy");
+    if (loginData.email.length === 0 || !emailRegex.test(loginData.email)) {
+      setErrors((prev) => ({
+        ...prev,
+        email: "Email jest nieprawidłowy",
+      }));
       return false;
     }
     return true;
   };
 
   const passwordValidation = () => {
-    if (password.length < 6) {
-      setPasswordErr("Hasło musi mieć conajmniej 6 znaków");
+    if (loginData.password.length < 6) {
+      setErrors((prev) => ({
+        ...prev,
+        password: "Hasło musi mieć co najmniej 6 znaków",
+      }));
       return false;
     }
     return true;
@@ -53,26 +60,28 @@ const Login = () => {
     <div className="login-wrapper">
       <Logo />
       <h2>Zaloguj się do SportLinker</h2>
-      <InputField label="Email" error={emailErr}>
+      <InputField label="Email" error={errors.email}>
         <Input
           placeholder="John@example.com"
           type="email"
           width={100}
           icon={<MdMailOutline color="rgb(156 163 175)" size="20px" />}
-          onChange={(e) => onEmailChange(e)}
-          value={email}
-          conditionalClass={emailErr}
+          onChange={(e) => handleChange(e)}
+          value={loginData.email}
+          conditionalClass={errors.email}
+          name="email"
         />
       </InputField>
-      <InputField label="Hasło" error={passwordErr}>
+      <InputField label="Hasło" error={errors.password}>
         <Input
           placeholder="••••••••"
           type="password"
           width={100}
           icon={<GoLock color="rgb(156 163 175)" size="20px" />}
-          onChange={(e) => onPasswordChange(e)}
-          value={password}
-          conditionalClass={passwordErr}
+          onChange={(e) => handleChange(e)}
+          value={loginData.password}
+          conditionalClass={errors.password}
+          name="password"
         />
       </InputField>
       <Button style="loginButton" width={100} onClick={loginSubmit}>
--- SportLinkerFrontend/src/components/authModal/Reigster.jsx
+++ SportLinkerFrontend/src/components/authModal/Reigster.jsx
@@ -6,37 +6,113 @@ import "./register.css";
 import { MdMailOutline } from "react-icons/md";
 import { GoLock } from "react-icons/go";
 import { LuUser } from "react-icons/lu";
+import { useState } from "react";
 
 const Register = () => {
+  const [loginData, setLoginData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prev) => ({ ...prev, [name]: value }));
+    setErrors((prev) => ({ ...prev, [name]: "" }));
+  };
+
+  const emailValidation = () => {
+    if (loginData.email.length === 0 || !emailRegex.test(loginData.email)) {
+      setErrors((prev) => ({
+        ...prev,
+        email: "Email jest nieprawidłowy",
+      }));
+      return false;
+    }
+    return true;
+  };
+
+  const nameValidation = () => {
+    if (loginData.name.trim().length === 0) {
+      setErrors((prev) => ({
+        ...prev,
+        name: "Imię i nazwisko nie może być puste",
+      }));
+      return false;
+    }
+    return true;
+  };
+
+  const passwordValidation = () => {
+    if (loginData.password.length < 6) {
+      setErrors((prev) => ({
+        ...prev,
+        password: "Hasło musi mieć co najmniej 6 znaków",
+      }));
+      return false;
+    }
+    return true;
+  };
+
+  const registerSubmit = () => {
+    const validatedName = nameValidation();
+    const validatedEmail = emailValidation();
+    const validatedPassword = passwordValidation();
+
+    if (validatedEmail && validatedPassword && validatedName) {
+      console.log("Zarejestrowano");
+    }
+  };
+
   return (
     <div className="register-wrapper">
       <Logo />
       <h2>Utwórz konto w SportLinker</h2>
-      <InputField label="Imię i nazwisko">
+      <InputField label="Imię i nazwisko" error={errors.name}>
         <Input
           placeholder="John Doe"
           type="text"
           width={100}
           icon={<LuUser color="rgb(156 163 175)" size="20px" />}
+          onChange={(e) => handleChange(e)}
+          value={loginData.name}
+          conditionalClass={errors.name}
+          name="name"
         />
       </InputField>
-      <InputField label="Email">
+      <InputField label="Email" error={errors.email}>
         <Input
           placeholder="John@example.com"
           type="email"
           width={100}
           icon={<MdMailOutline color="rgb(156 163 175)" size="20px" />}
+          onChange={(e) => handleChange(e)}
+          value={loginData.email}
+          conditionalClass={errors.email}
+          name="email"
         />
       </InputField>
-      <InputField label="Hasło">
+      <InputField label="Hasło" error={errors.password}>
         <Input
           placeholder="••••••••"
           type="password"
           width={100}
           icon={<GoLock color="rgb(156 163 175)" size="20px" />}
+          onChange={(e) => handleChange(e)}
+          value={loginData.password}
+          conditionalClass={errors.password}
+          name="password"
         />
       </InputField>
-      <Button style="loginButton" width={100}>
+      <Button style="loginButton" width={100} onClick={registerSubmit}>
         Utwórz konto
       </Button>
     </div>
