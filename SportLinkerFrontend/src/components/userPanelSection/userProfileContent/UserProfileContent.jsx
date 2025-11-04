9967cb67aa89b9402e908ee150f8e90071bb0e36
Michu <104158118+Michuxx@users.noreply.github.com>
2025-11-04 12:32:33 +0000

    small refactor code

--- SportLinkerFrontend/src/components/userPanelSection/userProfileContent/UserProfileContent.jsx
+++ SportLinkerFrontend/src/components/userPanelSection/userProfileContent/UserProfileContent.jsx
@@ -1,5 +1,3 @@
-import "./userProfileContent.css";
-
 const UserProfileContent = ({ children }) => {
   return <div className="user-profile-content-wrapper">{children}</div>;
 };
--- SportLinkerFrontend/src/components/userPanelSection/userProfileContent/userOffers/nullOffers/NullOffers.jsx
+++ SportLinkerFrontend/src/components/userPanelSection/userProfileContent/userOffers/nullOffers/NullOffers.jsx
@@ -3,6 +3,7 @@ import Button from "../../../../component-items/button/button";
 import ModalBackground from "../../../../component-items/modal/ModalBackground";
 import NullComponent from "../../../../component-items/nullComponent/NullComponent";
 import CreateOfferModal from "../../../../createOfferModal/CreateOfferModal";
+import "./nullOffers.css";
 
 const NullOffers = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
