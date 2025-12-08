ef8b93025af176cf158ad8c20b69ef9075de9d66
Michu <104158118+Michuxx@users.noreply.github.com>
2025-12-08 13:02:00 +0000

    small style changes and added member coutner

--- SportLinkerFrontend/src/components/offerPageSection/offerPageContent/offerAttendees/OfferAttendees.jsx
+++ SportLinkerFrontend/src/components/offerPageSection/offerPageContent/offerAttendees/OfferAttendees.jsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import "./offerAttendees.css";
 import OfferAuthorCard from "./offerAuthorCard/OfferAuthorCard";
 import OfferMemberCard from "./offerMemberCard/OfferMemberCard";
 
 const OfferAttendees = ({ author, members, offerAuthorId }) => {
+  const [peopleCounter, setPeopleCoutner] = useState();
+
+  useEffect(() => {
+    let counter = 1;
+    counter += members.length;
+    setPeopleCoutner(counter);
+  }, []);
+
   return (
     <div className="offer-attendees-container">
       <OfferAuthorCard author={author} />
       <div className="offer-member-list">
+        <h3>Uczestnicy ({peopleCounter})</h3>
         <OfferMemberCard member={author} offerAuthorId={offerAuthorId} />
         {members.map((member) => (
           <OfferMemberCard member={member} offerAuthorId={offerAuthorId} />
