dda78273a98acf460d55fba4b360c2f48e830e97
Michu <104158118+Michuxx@users.noreply.github.com>
2025-12-08 12:52:55 +0000

    styled member Card

--- SportLinkerFrontend/src/components/offerPageSection/offerPageContent/offerAttendees/offerMemberCard/OfferMemberCard.jsx
+++ SportLinkerFrontend/src/components/offerPageSection/offerPageContent/offerAttendees/offerMemberCard/OfferMemberCard.jsx
@@ -1,7 +1,20 @@
 import "./offerMemberCard.css";
+import Avatar from "../../../../component-items/avatar/Avatar";
 
-const OfferMemberCard = ({ member }) => {
-  return <div className="offer-member-card-container"></div>;
+const OfferMemberCard = ({ member, offerAuthorId }) => {
+  return (
+    <div className="offer-member-card-container">
+      <Avatar size={3} />
+      <div className="member-info-wrapper">
+        <p className="member-name">{member.name}</p>
+        {member.id === offerAuthorId ? (
+          <p className="author-member">Organizator</p>
+        ) : (
+          <p className="member-join-date">{member.joinDate}</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default OfferMemberCard;
