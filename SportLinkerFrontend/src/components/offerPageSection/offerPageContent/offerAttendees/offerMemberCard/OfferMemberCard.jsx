5969d41df04ff2764eecc1ddbf85c2db173a73ee
Michu <104158118+Michuxx@users.noreply.github.com>
2025-12-09 13:41:12 +0000

    delete button in offer page

--- SportLinkerFrontend/src/components/offerPageSection/offerPageContent/offerAttendees/offerMemberCard/OfferMemberCard.jsx
+++ SportLinkerFrontend/src/components/offerPageSection/offerPageContent/offerAttendees/offerMemberCard/OfferMemberCard.jsx
@@ -1,18 +1,30 @@
 import "./offerMemberCard.css";
 import Avatar from "../../../../component-items/avatar/Avatar";
+import Button from "../../../../component-items/button/button";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const OfferMemberCard = ({ member, offerAuthorId }) => {
   return (
     <div className="offer-member-card-container">
-      <Avatar size={3} />
-      <div className="member-info-wrapper">
-        <p className="member-name">{member.name}</p>
-        {member.id === offerAuthorId ? (
-          <p className="author-member">Organizator</p>
-        ) : (
-          <p className="member-join-date">{member.joinDate}</p>
-        )}
+      <div className="member-info-block">
+        <Avatar size={3} />
+        <div className="member-info-wrapper">
+          <p className="member-name">{member.name}</p>
+          {member.id === offerAuthorId ? (
+            <p className="author-member">Organizator</p>
+          ) : (
+            <p className="member-join-date">{member.joinDate}</p>
+          )}
+        </div>
       </div>
+      {member.id !== offerAuthorId && (
+        <div className="member-delete-btn">
+          <Button
+            style="deleteButton"
+            Icon={<FaRegTrashAlt size={20} />}
+          ></Button>
+        </div>
+      )}
     </div>
   );
 };
