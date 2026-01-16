69d31b6933b3c47dc8f83a0a6f5dbe5ed04375f2
Michu <104158118+Michuxx@users.noreply.github.com>
2026-01-16 11:40:48 +0000

    refactored Personal info components

--- SportLinkerFrontend/src/components/settingsSection/accountSettings/AccountSettings.jsx
+++ SportLinkerFrontend/src/components/settingsSection/accountSettings/AccountSettings.jsx
@@ -1,16 +1,7 @@
 import { useState } from "react";
 import UserDetailedBackground from "../../userPanelSection/userProfileContent/userInfo/userDetailed/UserDetailedBackground";
-import InputField from "../../component-items/inputField/InputField";
-import Input from "../../component-items/input/Input";
-import Select from "../../component-items/select/Select";
 import "./accountSettings.css";
-import { GENDERS } from "../../../assets/GENDERS";
-import { GrLocation } from "react-icons/gr";
-import { FiCalendar } from "react-icons/fi";
-import { MdMailOutline } from "react-icons/md";
-import { LuUser } from "react-icons/lu";
-import { FiPhone } from "react-icons/fi";
-import { FaTransgender } from "react-icons/fa";
+import PersonalInfo from "./PersonalInfo";
 
 const AccountSettings = () => {
   const [userInfo, setUserInfo] = useState({
@@ -57,19 +48,23 @@ const AccountSettings = () => {
     }
 
     // --- 2. BIRTH DATE ---
-    if (userInfo.birthDate > todaysDate) {
-      newErrors.birthDate = "Data urodzenia nie może być z przyszłości";
-      isValid = false;
-    } else if (userInfo.birthDate > tooYoungDate) {
-      newErrors.birthDate = "Musisz mieć co najmniej 13 lat";
-      isValid = false;
-    } else if (userInfo.birthDate < tooOldDate) {
-      newErrors.birthDate = "Wprowadzona data wskazuje na wiek powyżej 80 lat";
-      isValid = false;
+    if (userInfo.birthDate) {
+      if (userInfo.birthDate > todaysDate) {
+        newErrors.birthDate = "Data urodzenia nie może być z przyszłości";
+        isValid = false;
+      } else if (userInfo.birthDate > tooYoungDate) {
+        newErrors.birthDate = "Musisz mieć co najmniej 13 lat";
+        isValid = false;
+      } else if (userInfo.birthDate < tooOldDate) {
+        newErrors.birthDate =
+          "Wprowadzona data wskazuje na wiek powyżej 80 lat";
+        isValid = false;
+      }
     }
 
     setErrors(newErrors);
 
+    console.log(userInfo.birthDate);
     if (isValid) {
       console.log("zapisano");
     }
@@ -81,82 +76,11 @@ const AccountSettings = () => {
         title="Informacje podstawowe"
         saveEdited={handleSaveData}
       >
-        <div className="acc-settings-input-wrapper">
-          <InputField label="Jan Kowalski">
-            <Input
-              placeholder="John Doe"
-              type="text"
-              width={100}
-              icon={<LuUser color="rgb(156 163 175)" size="20px" />}
-              onChange={(e) => handleChange(e)}
-              value={userInfo.name}
-              error={errors.name}
-              name="name"
-            />
-          </InputField>
-          <InputField label="Email">
-            <Input
-              placeholder="John@example.com"
-              type="email"
-              width={100}
-              icon={<MdMailOutline color="rgb(156 163 175)" size="20px" />}
-              onChange={(e) => handleChange(e)}
-              value={userInfo.email}
-              name="email"
-              disabled={true}
-            />
-          </InputField>
-        </div>
-        <div className="acc-settings-input-wrapper">
-          <InputField label="Numer telefonu">
-            <Input
-              icon={<FiPhone color="rgb(156 163 175)" size="20px" />}
-              placeholder="+48 123 456 789"
-              type="text"
-              width={100}
-              onChange={(e) => handleChange(e)}
-              name="phoneNumber"
-              value={userInfo.phoneNumber}
-              disabled={true}
-            />
-          </InputField>
-          <InputField label="Lokalizacja">
-            <Input
-              placeholder="np. Warszawa"
-              type="text"
-              icon={<GrLocation color="rgb(156, 163, 175)" size={20} />}
-              width={100}
-              onChange={(e) => handleChange(e)}
-              name="location"
-              value={userInfo.location}
-            />
-          </InputField>
-        </div>
-        <div className="acc-settings-input-wrapper">
-          <InputField label="Data Urodzenia">
-            <Input
-              type="date"
-              icon={<FiCalendar color="rgb(156, 163, 175)" size={20} />}
-              width={100}
-              onChange={(e) => handleChange(e)}
-              name="birthDate"
-              value={userInfo.birthDate}
-              error={errors.birthDate}
-            />
-          </InputField>
-          <InputField label="Płeć">
-            <Select
-              icon={<FaTransgender color="rgb(156, 163, 175)" size={20} />}
-              width={100}
-              options={GENDERS}
-              defaultValue="NotDisclosed"
-              defaultText="Wolę nie podawać"
-              onChange={(e) => handleChange(e)}
-              name="gender"
-              value={userInfo.gender}
-            />
-          </InputField>
-        </div>
+        <PersonalInfo
+          userInfo={userInfo}
+          errors={errors}
+          handleChange={handleChange}
+        />
       </UserDetailedBackground>
     </div>
   );
