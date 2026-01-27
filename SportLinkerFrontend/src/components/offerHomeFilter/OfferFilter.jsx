cc94648eb6c8aed3e007c0613118150833c9ecdc
Michu <104158118+Michuxx@users.noreply.github.com>
2026-01-27 15:30:40 +0000

    Togglind btn filter

--- SportLinkerFrontend/src/components/offerHomeFilter/OfferFilter.jsx
+++ SportLinkerFrontend/src/components/offerHomeFilter/OfferFilter.jsx
@@ -2,8 +2,14 @@ import "./offerFilter.css";
 import { LuFilter } from "react-icons/lu";
 import SimpleOfferFilter from "./SimpleOfferFilter.jsx";
 import Button from "../component-items/button/button.jsx";
+import AdvancedOfferFilter from "./AdvancedOfferFilter.jsx";
+import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+import { useState } from "react";
 
 const OfferFilter = ({ filter, onChangeFilter }) => {
+  const [showAdvancedFilter, setShowAdvancedFilter] = useState(false);
+
   return (
     <div className="filter-wrapper">
       <div className="filter-headline-wrapper">
@@ -11,10 +17,19 @@ const OfferFilter = ({ filter, onChangeFilter }) => {
           <LuFilter color="#3b82f6" size="24px" />
           <h3>Filtruj oferty</h3>
         </div>
-        <Button>Zaawansowane</Button>
+        <Button onClick={() => setShowAdvancedFilter((e) => !e)}>
+          Zaawansowane
+          {showAdvancedFilter ? <IoIosArrowUp /> : <IoIosArrowDown />}
+        </Button>
       </div>
       <div className="filter-input-wrapper">
         <SimpleOfferFilter filter={filter} onChangeFilter={onChangeFilter} />
+        {showAdvancedFilter && (
+          <AdvancedOfferFilter
+            filter={filter}
+            onChangeFilter={onChangeFilter}
+          />
+        )}
       </div>
     </div>
   );
