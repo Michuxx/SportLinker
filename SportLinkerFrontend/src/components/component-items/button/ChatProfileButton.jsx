08521e6bb67c887534176eeb49c22952d5b2c0f3
Michu <104158118+Michuxx@users.noreply.github.com>
2026-02-23 15:44:06 +0000

    Added Button with info about users

---
+++ SportLinkerFrontend/src/components/component-items/button/ChatProfileButton.jsx
@@ -0,0 +1,21 @@
+import Avatar from "../avatar/Avatar";
+import "./chatProfileButton.css";
+
+const ChatProfileButton = ({ onClick, name, date, text }) => {
+  return (
+    <button className="chat-profile-btn" onClick={onClick}>
+      <div className="chat-profile-wrapper">
+        <Avatar size={2} />
+        <div className="chat-profile-info-wrapper">
+          <div className="chat-profile-profile-info">
+            <h3>{name}</h3>
+            <span>{date}</span>
+          </div>
+          <p>{text}</p>
+        </div>
+      </div>
+    </button>
+  );
+};
+
+export default ChatProfileButton;
--- SportLinkerFrontend/src/components/messagesSection/chatSidebar/conversationSidebar/ConversationSidebar.jsx
+++ SportLinkerFrontend/src/components/messagesSection/chatSidebar/conversationSidebar/ConversationSidebar.jsx
@@ -1,9 +1,20 @@
 import { useOutletContext } from "react-router";
 import "./conversationSidebar.css";
+import ChatProfileButton from "../../../component-items/button/ChatProfileButton";
 
 const ConversationSidebar = () => {
   const { chatPeople, onChatSelect } = useOutletContext();
-  return <div className="conversation-sidebar-wrapper"></div>;
+  return (
+    <div className="conversation-sidebar-wrapper">
+      {chatPeople.conversations.map((conversation) => (
+        <ChatProfileButton
+          name={conversation.name}
+          date={conversation.lastMessage.time}
+          text={conversation.lastMessage.text}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ConversationSidebar;
