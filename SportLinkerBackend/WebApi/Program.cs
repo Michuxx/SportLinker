a7575263bf057166a6fed1ca6a879fa4f2436028
Michuxx <104158118+Michuxx@users.noreply.github.com>
2026-09-21 00:05:31 +0200

    new start

---
+++ SportLinkerBackend/WebApi/Program.cs
@@ -0,0 +1,37 @@
+using Application;
+using Infrastructure;
+using Presentation;
+using Scalar.AspNetCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+builder.Services
+        .AddApplication()
+        .AddInfrastructure()
+        .AddPresentation();
+
+builder.Host.UseSerilog((context, conf) =>
+    conf.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
