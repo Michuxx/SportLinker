06a86537d77779e018439d2b2b3bfebcbccfaeb5
Michuxx <104158118+Michuxx@users.noreply.github.com>
2026-09-21 20:30:20 +0200

    Initial Entites

---
+++ SportLinkerBackend/Domain/Entities/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string? Country { get; set; }
+        public string? State { get; set; }
+        public string? City { get; set; }
+        public string? Name { get; set; }
+        public string? Street { get; set; }
+        public string? HouseNumber { get; set; }
+        public string? Type { get; set; }
+
+    }
+}
---
+++ SportLinkerBackend/Domain/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public DateOnly? BirthDate { get; set; }
+        public string? Gender { get; set; }
+        public string? AboutMe { get; set; } 
+        public string? ProfileImage { get; set; }
+        public string? BackgroundImage { get; set; } 
+        public int? PhoneNumber { get; set; }
+        public int? PhoneExtension { get; set; }
+        public int CreatedOffers { get; set; }
+        public int JoinedOffers { get; set; }
+        public int Invitations { get; set; }
+        public int? LocationId { get; set; }
+        public Location? Location { get; set; }
+        public string Role { get; set; } = "User";
+    }
+}
